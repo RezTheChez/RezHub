a221c68c4c79f2d829fd4d403d4418b0a1110b4a
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2022-11-27 09:50:53 -0800

    Added NoClip.lua

---
+++ NoClip.lua
@@ -0,0 +1,30 @@
+if Value then
+			while true do
+				player = game.Players.LocalPlayer
+				character = player.Character
+				for _, v in pairs(character:GetDescendants()) do
+					pcall(function()
+						if v:IsA("BasePart") then
+							v.CanCollide = false
+						end
+					end)
+				end
+
+				game:GetService("RunService").Stepped:wait()
+			end
+		else
+			while true do
+				player = game.Players.LocalPlayer
+				character = player.Character
+
+				for _, v in pairs(character:GetDescendants()) do
+					pcall(function()
+						if v:IsA("BasePart") then
+							v.CanCollide = true
+						end
+					end)
+				end
+
+				game:GetService("RunService").Stepped:wait()
+			end
+		end
