454b61ed0764597f04f7d5691d22bf3d13db5773
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2023-01-13 13:06:34 -0800

    Update ChooseUI.lua

--- universal scripts/ChooseUI.lua
+++ universal scripts/ChooseUI.lua
@@ -7,11 +7,18 @@ local kavo = Instance.new("TextButton")
 local discord = Instance.new("TextButton")
 local text = Instance.new("TextLabel")
 local title = Instance.new("TextLabel")
+local rc = Instance.new("UICorner")
+local kc = Instance.new("UICorner")
+local dc = Instance.new("UICorner")
 
 local closeC = 150
 local closeH = false
 local rayfieldC = 150
 local rayfieldH = false
+local kavoC = 150
+local kavoH = false
+local discordC = 150
+local discordH = false
 
 Main.Name = "Main"
 Main.Enabled = true
@@ -51,7 +58,6 @@ text.Name = "text"
 text.Parent = notification
 text.BackgroundColor3 = Color3.fromRGB(34, 87, 168)
 text.BackgroundTransparency = 1.000
-text.BorderColor3 = Color3.fromRGB(27, 42, 53)
 text.BorderSizePixel = 0
 text.Position = UDim2.new(0, 0, 0, 20)
 text.Size = UDim2.new(0, 400, 0, 60)
@@ -65,22 +71,100 @@ rayfield.Name = "text"
 rayfield.Parent = notification
 rayfield.BackgroundColor3 = Color3.fromRGB(24, 24, 24)
 rayfield.BorderSizePixel = 0
-rayfield.Position = UDim2.new(0.2, 0, 0, 100)
+rayfield.Position = UDim2.new(0.1, 0, 0, 100)
 rayfield.Size = UDim2.new(0.2, 0, 0, 60)
+rayfield.TextScaled = true
 rayfield.ZIndex = 2
 rayfield.Font = Enum.Font.Ubuntu
-rayfield.Text = "Rayfield"
+rayfield.TextColor3 = Color3.fromRGB(150, 150, 150)
+rayfield.Text = "Rayfield UI"
 
+-- Kavo Button
 
 kavo.Name = "Kavo"
 kavo.Parent = notification
 kavo.BackgroundColor3 = Color3.fromRGB(24, 24, 24)
 kavo.BorderSizePixel = 0
-kavo.Position = UDim2.new(0.5, 0, 0, 100)
+kavo.Position = UDim2.new(0.4, 0, 0, 100)
 kavo.Size = UDim2.new(0.2, 0, 0, 60)
+kavo.TextScaled = true
 kavo.ZIndex = 2
 kavo.Font = Enum.Font.Ubuntu
-kavo.Text = "Kavo"
+kavo.TextColor3 = Color3.fromRGB(150, 150, 150)
+kavo.Text = "Kavo UI"
+
+-- Discord Button
+
+discord.Name = "Discord"
+discord.Parent = notification
+discord.BackgroundColor3 = Color3.fromRGB(24, 24, 24)
+discord.BorderSizePixel = 0
+discord.Position = UDim2.new(0.7, 0, 0, 100)
+discord.Size = UDim2.new(0.2, 0, 0, 60)
+discord.TextScaled = true
+discord.ZIndex = 2
+discord.Font = Enum.Font.Ubuntu
+discord.TextColor3 = Color3.fromRGB(150, 150, 150)
+discord.Text = "Discord UI"
+
+-- Rayfield Corner
+
+rc.Name = "UICorner"
+rc.CornerRadius = Udim2.new(3)
+rc.Parent = rayfield
+
+kc.Name = "UICorner"
+kc.CornerRadius = Udim2.new(3)
+kc.Parent = kavo
+
+dc.Name = "UICorner"
+dc.CornerRadius = Udim2.new(3)
+dc.Parent = discord
+
+
+discord.MouseEnter:Connect(function()
+    discordH = true
+    if discordC < 255 then
+      repeat 
+        wait(0.02)
+        discordC += 5
+        discord.TextColor3 = Color3.fromRGB(discordC, discordC, discordC)
+      until discordC == 255 or discordH == false
+    end
+end)
+
+discord.MouseLeave:Connect(function()
+    discordH = false
+    if discordC > 150 then
+      repeat 
+        wait(0.02)
+        discordC -= 5
+        discord.TextColor3 = Color3.fromRGB(discordC, discordC, discordC)
+      until discordC == 150 or discordH == true
+    end
+end)
+
+kavo.MouseEnter:Connect(function()
+    discordH = true
+    if discordC < 255 then
+      repeat 
+        wait(0.02)
+        discordC += 5
+        discord.TextColor3 = Color3.fromRGB(discordC, discordC, discordC)
+      until discordC == 255 or discordH == false
+    end
+end)
+
+kavo.MouseLeave:Connect(function()
+    kavoH = false
+    if kavoC > 150 then
+      repeat 
+        wait(0.02)
+        kavoC -= 5
+        kavo.TextColor3 = Color3.fromRGB(kavoC, kavoC, kavoC)
+      until kavoC == 150 or kavoH == true
+    end
+end)
 
 rayfield.MouseEnter:Connect(function()
     rayfieldH = true
@@ -130,4 +214,19 @@ end)
 
 close.MouseButton1Click:Connect(function()
     Main:Destroy()
+end)
+
+rayfield.MouseButton1Click:Connect(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/RezTheChez/RezHub/main/scripts/UniversalHubRayfield.lua"))
+        Main:Destroy()
+end)
+
+kavo.MouseButton1Click:Connect(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/RezTheChez/RezHub/main/scripts/UniversalHubKavo.lua"))
+        Main:Destroy()
+end)
+
+discord.MouseButton1Click:Connect(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/RezTheChez/RezHub/main/scripts/UniversalHubDiscord.lua"))
+        Main:Destroy()
 end)
