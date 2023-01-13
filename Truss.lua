89f25a1a4e6c1fbed6a1abc08897945298c7ddf4
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2023-01-13 11:11:55 -0800

    Delete Truss.lua

--- Truss.lua
+++
@@ -1,5 +0,0 @@
-local truss = Instance.new("TrussPart", game:GetService("Workspace"))
-truss.Position = character.LeftFoot.Position
-truss.Size = Vector3.new(2, 64, 2)
-truss.Orientation = Vector3.new(0, 0, 0)
-truss.Anchored = true
