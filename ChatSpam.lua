a4c2ba6f03214e1ce5115463305e857a4bd5edf5
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2023-01-13 09:55:59 -0800

    Delete ChatSpam.lua

--- ChatSpam.lua
+++
@@ -1,8 +0,0 @@
-while wait(0.1) do
-			local A_1=chatMesage
-			local A_2=chatMesage
-			local Event=game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
-			while wait(chatInerval) do
-				Event:FireServer (A_1, A_2)
-			end
-		end
