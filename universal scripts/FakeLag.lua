c3f02d8f3bb7c71ed2576e25793814d084aa6382
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2023-01-13 09:56:58 -0800

    Create FakeLag.lua

---
+++ universal scripts/FakeLag.lua
@@ -0,0 +1,11 @@
+while wait(0.1) do
+	if Value then
+		game.Players.LocalPlayer.Character.LowerTorso.Anchored = true
+		game.Players.LocalPlayer.Character.Humanoid.Jump = true
+    
+		wait(0.1)
+    
+		game.Players.LocalPlayer.Character.LowerTorso.Anchored = false
+		game.Players.LocalPlayer.Character.Humanoid.Sit = true
+	end
+end
