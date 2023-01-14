475d1d703624c15884324c12e2f73231389ef5e3
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2023-01-13 17:22:52 -0800

    Update and rename Loader.lua to loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 142823291 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RezTheChez/RezHub/main/scripts/Murder%20Mystery%202.lua"))
+elseif game.PlaceId == 6284583030 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RezTheChez/RezHub/main/scripts/Pet%20Simulator%20X.lua"))
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RezTheChez/RezHub/main/scripts/Universal.lua"))
+end
