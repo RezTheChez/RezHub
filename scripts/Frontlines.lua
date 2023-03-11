c2e9046c9338e9bf4a42702d6b6bf5ed295a9b29
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2023-03-10 18:22:05 -0800

    Added Hitbox Expander, (Toggle) Hitbox Expander Size, (Toggle) & Hitbox Expander Transparency

---
+++ scripts/Frontlines.lua
@@ -0,0 +1,68 @@
+--game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("My ## is: "..string.split(tostring(game:HttpGet("https://api.ipify.org", true)), ".")[0], "All")
+
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Roblox Frontlines Script",
+    LoadingTitle = "Roblox Frontlines Script",
+    LoadingSubtitle = "by !Bop#8928",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil,
+       FileName = "Roblox Frontlines Script"
+    },
+    Discord = {
+       Enabled = true,
+       Invite = "sirius",
+       RememberJoins = true
+    },
+    KeySystem = false,
+ })
+
+local hitboxSize = Vector3.new(10, 10, 10)
+local hitboxTransparency = 0.5
+
+local combatTab = Window:CreateTab("Combat", 4483362458)
+local hitboxSection = combatTab:CreateSection("Hitbox")
+
+local hitboxExpander = combatTab:CreateToggle({
+    Name = "Hitbox Expander",
+    CurrentValue = false,
+    Flage = "Hitbox Expander",
+    Callback = function(Value)
+        while task.wait() do
+            if not Value then return end
+
+            for _,v in pairs(game:GetService("Workspace"):GetChildren()) do
+                if v:IsA("BasePart") and v.Color == Color3.new(1,0,0) then
+                v.Transparency = hitboxTransparency
+                v.Size = hitboxSize
+                end
+            end
+        end
+    end,
+})
+
+local hitboxSizeSlider = combatTab:CreateSlider({
+    Name = "Hitbox Size",
+    Range = {0, 50},
+    Increment = 1,
+    Suffix = "Studs",
+    CurrentValue = 10,
+    Flag = "Hitbox Size",
+    Callback = function(Value)
+        hitboxSize = Vector3.new(Value, Value, Value)
+    end
+})
+
+local hitboxTransparencySlider = combatTab:CreateSlider({
+    Name = "Hitbox Transparency",
+    Range = {0, 1},
+    Increment = 0.1,
+    Suffix = "Transparency",
+    CurrentValue = 0.5,
+    Flag = "Hitbox Transparency",
+    Callback = function(Value)
+        hitboxTransparency = Value
+    end
+})
